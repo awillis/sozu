9a0d6181f553154251a8173b92c4368db91e4b2a
Alan Willis <jiggorz@amekoshi.com>
2020-12-16 15:55:17 -0800

    Initial commit

---
+++ README.rst
@@ -0,0 +1,16 @@
+===================
+sozu threat modeler
+===================
+
+---------------------------------------
+threat modeling tool inspired by MSTMT_
+---------------------------------------
+
+All models are wrong. Some are very useful.
+
+Cross platform threat modeling tool using data flow diagrams.
+Written in python3.9 / qt6
+
+Aims to enable import of MSTMT_ models
+
+.. _MSTMT: https://docs.microsoft.com/en-us/azure/security/develop/threat-modeling-tool
---
+++ setup.py
@@ -0,0 +1,50 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="sozu",
+    version="0.0.1",
+    description="cross-platform threat modeling tool",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/awillis/sozu",
+    project_urls={
+        "Bug Reports": "https://github.com/awillis/sozu/issues",
+        "Source": "https://github.com/awillis/sozu"
+    },
+    author="Alan Willis",
+    author_email="alan@amekoshi.com",
+    license="Apache-2.0",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Security"
+    ],
+    keywords="threat model infosec security dfd",
+    packages=find_packages(include=['sozu']),
+    python_requires=">=3.9, <4",
+    include_package_data=True,
+    install_requires=[],
+    data_files=[],
+    entry_points={
+        'console_scripts': [
+            'sozu=sozu.interface:cli'
+        ],
+        'gui_scripts': [
+            'sozu=sozu.interface:gui'
+        ]
+    }
+)
